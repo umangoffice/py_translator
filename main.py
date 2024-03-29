56b9f9652caeeabe0e3428dee4c3572938799cba
Umang <umang16office@gmail.com>
2024-03-29 17:17:32 +0530

    completely working

--- main.py
+++ main.py
@@ -21,8 +21,8 @@ label.grid(row=0, column=0)
 entry = Entry(root, width=50)
 entry.grid(row=1, column=0, padx=10)
 
-submit_button = Button(root, text="Submit", command=handle_input)
-submit_button.grid(row=1, column=1, padx=5)
+submit_b1 = Button(root, text="Submit", command=handle_input, width=7)
+submit_b1.grid(row=1, column=1, padx=5)
 
 languages = {"afrikaans":"af", "albanian": "sq", "amharic":"am", "arabic":"ar", "armenian":"hy", "azerbaijani":"az", "basque":"eu", "belarusian":"be", "bengali":"bn", "bosnian":"bs", "bulgarian":"bg", "catalan":"ca", "cebuano":"ceb", "chichewa":"ny", "chinese traditional": "zh-tw", "chinese simplified": "zh-cn", "corsican":"co", "croatian":"hr", "czech":"cs", "danish":"da", "dutch":"nl", "english":"en", "esperanto":"eo", "filipino":"tl", "estonian":"et", "finnish":"fi", "french":"fr", "frisian":"fy", "galician":"gl", "georgian":"ka", "german":"de", "greek":"el", "gujarati":"gu", "haitian creole": "ht", "hausa":"ha", "hawaiian":"haw", "hebrew":"he", "hindi":"hi", "hmong":"hmn", "hungarian":"hu", "icelandic":"is", "igbo":"ig", "indonesian":"id", "irish":"ga", "italian":"it", "japanese":"ja", "javanese":"jw", "kannada":"kn", "kazakh":"kk", "khmer":"kh", "korean":"ko", "kurdish":"ku", "kyrgyz":"ky", "lao":"lo", "latin":"la", "latvian":"lv", "lithuanian":"lt", "louxembourgish":"lb", "macedonian":"mk", "malagasy":"mg", "malay":"ms", "malayalam":"ml", "maltese":"mt", "marathi": "mi", "mongolian":"mn", "myanmar":"me", "norwegian":"no", "nepali":"ne", "odia":"or", "pashto":"ps", "persian":"fa", "polish":"pl", "portugese":"pt", "punjabi":"pa", "romanian":"ro", "russian":"ru", "samoan":"sm", "scots gaelic":"gd", "serbian":"sr", "sesotho":"st", "shona":"sn", "sindhi":"sd", "sinhala":"si", "slovak":"sk", "slovenian":"sl", "somali":"so", "spanish":"es", "sudanese":"su", "swhili":"sw", "swedish":"sv", "tajik":"tg", "tamil":"ta", "telugu":"te", "thai":"th", "turkish":"tr", "ukrainian":"uk", "urdu":"ur", "uyghur":"ug", "uzbek":"uz", "vietnamese":"vi", "welsh":"cy", "xhosa":"xh", "yiddish":"yi", "yoruba":"yo", "zulu":"zu"}
 
@@ -44,20 +44,26 @@ def find_abbrev():
             lang_abbrev = languages[each_lang]
             break
     
-done = Button(root, text="Submit", command=find_abbrev)
-done.grid(row=4, column=1, padx=5)
+submit_b2 = Button(root, text="Select", command=find_abbrev, width=7)
+submit_b2.grid(row=4, column=1, padx=5)
 
-result = "Translation here:\t"
+result = ""
 def translate():
     result = translator.translate(user_input, dest=lang_abbrev).text
     T.insert(tk.END, result)
     
-T = Text(root, height=10, width=30)
-T.grid(row=7, column=0, padx=10)
+T = Text(root, height=7, width=38)
+T.grid(row=5, column=0, padx=10, pady=20)
 
 T.insert(tk.END, result)
 
-go = Button(root, text="Go", command=translate)
-go.grid(row=6, column=0, padx=5)
+def copy_to_clipboard():
+    pyperclip.copy(T.get(1.0, "end-1c"))
+
+submit_b4 = Button(root, text="Translate", command=translate, width=25)
+submit_b4.grid(row=6, column=0 , padx=5)
+
+submit_b5 = Button(root, text="Copy", command=copy_to_clipboard, width=7)
+submit_b5.grid(row=5, column=1, padx=5, pady = 5)
 
 root.mainloop()
